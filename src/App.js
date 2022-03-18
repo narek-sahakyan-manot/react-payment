d9b1603be38e4139f18c93c70b234a70fadc1644
Narek Sahakyan <narek@manot.tech>
2022-03-18 18:12:16 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Components
+import HomePage from './components/HomePage';
+// Styles
+import './index.css';
+// Stripe Imports
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+// the key is located in the .env file
+const stripePromise = loadStripe("pk_live_51JPNDnL1qENZFgB9f3eiGevN9E6JzPNr5xmUGLqZrCmknnuh6ubmVNtSvcazN8dnvY7q0LnLRTApmFxPH2baIhcV00RyCiYmMH");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <HomePage />
+    </Elements>
   );
 }
 
